7a73db026f8f31212123a299cf10e1d740b1f66d
HallanaKeury <hallanasousa10@gmail.com>
2022-07-26 16:03:23 -0300

    Primeiro programa hello world + soma

---
+++ primeiro-programa-java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =2;
+		int b =5;
+		
+		System.out.println("Hello world!" + (a+b));
+		
+		
+
+	}
+
+}
